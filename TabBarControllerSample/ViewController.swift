606e71de1994c845205760228248c7bc09526907
Ryusuke Kawasaki <kawasaki@codable.co.jp>
2016-11-18 19:00:11 +0900

    first commit

---
+++ TabBarControllerSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabBarControllerSample
+//
+//  Created by 川崎 隆介 on 2015/12/10.
+//  Copyright (c) 2015年 川崎 隆介. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
